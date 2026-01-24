2b3b39e2dacb61756a269153479bc1aeef524f72
Adrian Fernandez <adrian23fernandez@gmail.com>
2026-01-23 21:08:39 -0500

    Wrote Python Driver and tested it with the pico

---
+++ driver/serial-message-driver.py
@@ -0,0 +1,216 @@
+import serial
+import struct
+from crc import Calculator, Crc8
+
+class SerialMsg:
+    SYNC = 0x6721
+    PAYLOAD_MAX_LEN = 8
+    __crc_calc = Calculator(Crc8.CCITT) # if you didnt know, calc is short for calculator
+
+    def __init__(self, action : int, payload : bytes | list[int], sync : int = SYNC):
+        """
+        __init__
+        
+        :param action: Action to perfom, 1 byte
+        :type action: int
+        :param payload: Bytes array to send, up to 8 bytes
+        :type payload: bytes
+        :param sync: Optional, Sync (aka start delimiters) bytes, 2 bytes
+        :type sync: int
+        """
+        if not isinstance(sync, int):
+            raise TypeError("Sync must be int")
+        if not isinstance(action, int):
+            raise TypeError("Action must be int")
+        
+        if not isinstance(payload, (bytes, bytearray, list)):
+            raise TypeError("payload must be bytes, bytearray, or list[int]")
+        if isinstance(payload, list):
+            if not all(isinstance(b, int) and 0 <= b <= 255 for b in payload):
+                raise ValueError("payload list elements must be ints in range 0..255")
+
+        self.sync = sync & 0xFFFF
+        self.action = action & 0xFF
+        self.payload = bytes(payload[:SerialMsg.PAYLOAD_MAX_LEN])
+
+    def convert_msg_to_bytes(self) -> bytes:
+        '''
+        Method that converts the current message to a byte array. Typically to send it through a serial device.
+        
+        :return: bytes array of the message
+        :rtype: bytes
+        '''
+        sync_bytes = struct.pack(">H", self.sync & 0xFFFF)
+        middle_bytes = struct.pack("<BB", self.action & 0xFF, len(self.payload) & 0xFF) + self.payload
+
+        crc8 = self.get_checksum()
+        crc8_byte = struct.pack("<B", crc8 & 0xFF)
+
+        return sync_bytes + middle_bytes + crc8_byte
+
+    def get_checksum(self) -> int:
+        '''
+        Method that calculates the checksum of the current message
+        
+        :return: Description
+        :rtype: int
+        '''
+        length = len(self.payload)
+        data = struct.pack("<BB", self.action & 0xFF, length & 0xFF) + self.payload
+        return SerialMsg.__crc_calc.checksum(data)
+
+
+class PicoSerial:
+    '''
+    PicoSerial Class
+    Public Variables:
+    - ser : Serial object, serial device connected to
+    - sent_buf : bytes, buffer of previous message sent
+    - received_buf : bytes, buffer of previous message received
+    - logging : bool, if True, prints logging messages during Tx or Rx
+
+    Public Functions: 
+    - connect: TODO
+    - close: closes serial device
+    - send_msg: sends a message to serial device
+    - read_msg: receives a message from serial device 
+    '''
+
+    def __init__(self, port : str, baud : int = 115200, timeout : float = 0.1, logging : bool = False):
+        '''
+        __init__
+        
+        :param port: string of device to connect to
+        :type port: str
+        :param baud: baudrate of serial device, default is 115200
+        :type baud: int
+        :param timeout: timeout value (in seconds) a serial device sound wait
+        :type timeout: float
+        :param logging: boolean that enables log messages when sending or receiving
+        :type logging: bool
+        '''
+        self.ser = serial.Serial(port, baud, timeout=timeout)
+        self.sent_buf = bytes()
+        self.received_buf = bytes()
+        self.logging = logging
+        pass
+
+    def __del__(self):
+        self.ser.close()
+
+    def connect(self):
+        '''
+        TODO: Docstring for connect
+        
+        :param self: Description
+        '''
+        # TODO
+        pass 
+
+    def close(self) -> None:
+        '''
+        Closes the serial device
+        '''
+        self.ser.close()
+
+    def __stringify(self, bytesarr : bytes | list[int]) -> str:
+        return ' '.join(f'x{byte:02x}' for byte in bytesarr)
+    
+    def send_msg(self, action : int, payload: bytes| list[int]) -> int:
+        '''
+        Sends a message to the serial device. Will print log message if object's logging is true.
+        
+        :param action: Action byte of the message
+        :type action: int
+        :param payload: Payload bytes array or list to send
+        :type payload: bytes | list[int]
+        :return: Status value if succeeded or not in sending. Any non-zero number means it failed.
+        :rtype: int
+        :rval:
+        '''
+        if len(payload) > SerialMsg.PAYLOAD_MAX_LEN:
+            return 1
+        
+        msg = SerialMsg(action, bytes(payload))
+        self.sent_buf = msg.convert_msg_to_bytes()
+
+        if (self.logging):
+            print(f"Sending: {self.__stringify(self.sent_buf)}")
+
+        self.ser.write(self.sent_buf)
+
+        return 0
+
+    def read_msg(self, as_hex : bool = False) -> list[int] | str:
+        '''
+        Reads from the serial device. Will print log message if object's logging is true.
+        
+        :param as_hex: Boolean for whether or not, the return type is a int list or the hex string version of it.
+        :type as_hex: bool
+        :return: Either a int list or hex string list of the read buffer
+        :rtype: list[int] | str
+        '''
+        self.received_buf = self.ser.read_until()
+        received_list = list(struct.unpack(f'{len(self.received_buf)}B', self.received_buf))
+
+        if as_hex:
+            result = self.__stringify(received_list)
+        else:
+            result = received_list
+
+        if self.logging:
+            print(f"Received: {result}")
+        return result
+
+
+
+if __name__ == "__main__":
+    PORT = "/dev/ttyACM0"
+
+    device = None
+    try: 
+        device = PicoSerial(PORT, logging=True)
+
+        stars = ("**********************************")
+        print(stars)
+        print("Testing Serial Power Motor Drivers")
+        print(stars)
+
+        while True:
+
+            action = input("\nEnter action code: ")
+
+            while not action.isdigit():
+                action = input("Invalid, Enter action code: ")
+
+            action = int(action)
+
+            payload = []
+
+            while True:
+                if len(payload) >= SerialMsg.PAYLOAD_MAX_LEN:
+                    print(f"Max payload size ({SerialMsg.PAYLOAD_MAX_LEN}) reached, ")
+                    break
+
+                val = input("Enter payload, blank to end: ").strip()
+                if val == "":
+                    break
+                if not val.isdigit():
+                    print(f"{val} is invalid.", end=" ")
+                    continue
+
+                payload.append(int(val))
+
+            print(f"action = {action}, payload = {payload}, length = {len(payload)}")
+
+            device.send_msg(action, payload)
+            
+            output = device.read_msg(True)        
+    finally:
+        print(stars)
+        print("Program is ending...")
+        print(stars)
+
+        if device is not None:
+            device.close()
+            print("Device closed")
--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
+#include "stdio.h"
 
-#include "serial-message.h"
+#include "pico-serial-message.h"
 
 int main() {
     stdio_init_all();
 
+    serial_msg_t msg;
+
     while(1) {
         tight_loop_contents();
+
+        error_t status = ReadSerialMsg(&msg);
+        if (SUCCESS == status) {
+            SendSerialMsg(&msg);
+        } else if (ERR_WRONG_SYNC != status) {
+            putchar_raw(0xFF);
+        }
     }
 }
